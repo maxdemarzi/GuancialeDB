2d3175dea353688d891d2146be5302cb0f724270
Max De Marzi <maxdemarzi@hotmail.com>
2017-01-02 18:10:57 -0600

    Results.noContent() does not get picked up by Swagger, so returning 204 manually

--- src/main/java/com/maxdemarzi/Server.java
+++ src/main/java/com/maxdemarzi/Server.java
@@ -72,11 +72,11 @@ public class Server extends Jooby {
                 /*
                  * Delete node by ID.
                  * @param id Node ID.
-                 * @return Returns <code>204</code>
+                 * @return Returns <code>204</code> or <code>404</code>
                  */
                 .delete ("/node/:id", req -> {
                     if (db.removeNode(req.param("id").value())) {
-                        return Results.noContent();
+                        return Results.with(204);
                     } else {
                         throw new Err(Status.NOT_FOUND);
                     }
@@ -103,14 +103,14 @@ public class Server extends Jooby {
                  * @param type Relationship Type.
                  * @param from Starting Node ID.
                  * @param to Ending Node ID.
-                 * @return Returns <code>204</code>
+                 * @return Returns <code>204</code> or <code>404</code>
                  */
                 .delete ("/relationship/:type/:from/:to", req -> {
                     if (db.removeRelationship(
                             req.param("type").value(),
                             req.param("from").value(),
                             req.param("to").value())) {
-                        return Results.noContent();
+                        return Results.with(204);
                     } else {
                         throw new Err(Status.NOT_FOUND);
                     }
